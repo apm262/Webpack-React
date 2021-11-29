1de54df581d2632f667596cd7e57ccef9f73e188
Alfonso <alfonsopm08@gmail.com@mail.com>
2021-11-29 19:23:29 +0100

    Segundo commit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   // webpack 5 comes with devServer which loads in development mode
  devServer: {
    port: 3000,
-   watchContentBase: true
+  // watchContentBase: true
  },
   // Rules of how webpack will take our files, complie & bundle them for the browser 
  module: {
